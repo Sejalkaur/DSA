7c01981b0494dcb0d21312d15b7a175d35397d1e
Sejal Kaur <sejalkaur2004@gmail.com>
2024-02-24 19:04:04 +0530

    Update Step1.1.1.cpp

--- Section01/Step1.1/Step1.1.1.cpp
+++ Section01/Step1.1/Step1.1.1.cpp
@@ -1,9 +1,9 @@
-//Link: https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
-// Problem statement
-//Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
-//1, if the character is an uppercase alphabet (A - Z).
-//0, if the character is a lowercase alphabet (a - z).
-//-1, if the character is not an alphabet.
+/*Link: https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+Problem statement
+Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z).
+0, if the character is a lowercase alphabet (a - z).
+-1, if the character is not an alphabet.*/
 
 #include <bits/stdc++.h>
 
