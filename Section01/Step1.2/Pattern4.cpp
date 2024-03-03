b1c5e4520249258d73bd42b7e61c425793822766
Sejal Kaur <sejalkaur2004@gmail.com>
2024-03-03 07:50:26 +0530

    Create Pattern4.cpp

---
+++ Section01/Step1.2/Pattern4.cpp
@@ -0,0 +1,10 @@
+//Link: https://www.codingninjas.com/studio/problems/triangle_6573690?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+void triangle(int n) {
+	// Write your code here
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+}
