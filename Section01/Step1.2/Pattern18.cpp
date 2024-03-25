b084d7d1c70925cca28d2a992f6443fed6b600e0
Sejal Kaur <sejalkaur2004@gmail.com>
2024-03-25 23:17:49 +0530

    Create Pattern18.cpp

---
+++ Section01/Step1.2/Pattern18.cpp
@@ -0,0 +1,16 @@
+/*E
+D E
+C D E
+B C D E
+A B C D E*/
+void alphaTriangle(int n) {
+    // Write your code here.
+    for(int i =0;i<n;i++){
+        
+		for(char ch='E'-i;ch<='E';ch++){
+			cout<< ch<<" ";
+			
+		}
+		cout<<endl;
+	}
+}
