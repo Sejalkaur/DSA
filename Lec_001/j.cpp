297a7e5c59186d5e90b71077a630e148adf320a0
Sejal Kaur <sejalkaur2004@gmail.com>
2024-04-17 12:40:27 +0530

    Add files via upload

---
+++ Lec_001/j.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+
+    int a, b;
+    cout<<"Enter the value of a"<< endl;
+    cin>>a;
+    cout<<"Enter the value of b"<< endl;
+    cin>>b;
+    if(a>b){
+        cout<<"A is greatrer than b"<<endl;
+    }else{
+        cout<<"B is greater than A"<<endl;
+    }
+
+}
