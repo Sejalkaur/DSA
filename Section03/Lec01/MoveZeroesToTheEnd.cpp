695fbec8fa896f6caa44dcd39ba18b18a73eb7e3
Sejal Kaur <sejalkaur2004@gmail.com>
2024-04-29 22:50:49 +0530

    Create MoveZeroesToTheEnd.cpp

---
+++ Section03/Lec01/MoveZeroesToTheEnd.cpp
@@ -0,0 +1,40 @@
+//Link: https://www.naukri.com/code360/problems/ninja-and-the-zero-s_6581958?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+//Brute:
+vector<int> moveZeros(int n, vector<int> arr) {
+    // Write your code here.
+    int i=0;
+	vector<int> temp;
+	vector<int> start;
+	for(int j=0;j<n;j++){
+		if(arr[j]!=0){ //for non-zeroes
+			start.push_back(arr[j]); 
+			i++; //for comparing
+		}else if(arr[j]==0){ //for zeroes
+			temp.push_back(arr[j]);
+		}
+		
+	}start.insert(start.end(), temp.begin(), temp.end()); //to merge the two arrays
+	return start;
+}
+
+
+//Optimal:
+vector<int> moveZeros(int n, vector<int> arr) {
+    // Write your code here.
+  int j=-1;
+  for(int i=0;i<n;i++){
+	  if(arr[i]==0){
+		  j=i; //store their indices
+		  break;
+	  }
+  }
+  if(j==-1) return arr; 
+  for(int i=j+1;i<n;i++){
+	  if(arr[i]!=0){
+		  swap(arr[i], arr[j]);
+		  j++;
+	  }
+  }
+  return arr;
+}
