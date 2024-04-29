7131a79a457dca2acc29e9e2f9235fa0ab43ad34
Sejal Kaur <sejalkaur2004@gmail.com>
2024-04-29 11:23:13 +0530

    Update CheckSortedArray.cpp

--- Section03/Lec01/CheckSortedArray.cpp
+++ Section03/Lec01/CheckSortedArray.cpp
@@ -1,14 +1,14 @@
 //Link: https://www.naukri.com/code360/problems/ninja-and-the-sorted-check_6581957?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
 int isSorted(int n, vector<int> a) {
-    // Write your code here.
-    int ans=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i]<a[j]){
-                ans=1;
-            }else{
-                ans= 0;
-            }
-        }return ans;
-    }
+  // Write your code here.
+  
+  for(int i=1;i<n;i++){
+      if(a[i] >= a[i-1]){
+          
+      }else{
+          return false;
+      }
+  }return true;
+  
 }
+
