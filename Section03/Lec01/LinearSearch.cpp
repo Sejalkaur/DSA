d798680e1b5fb008a95d987fa706bbe68a3ca969
Sejal Kaur <sejalkaur2004@gmail.com>
2024-04-30 22:08:36 +0530

    Create LinearSearch.cpp

---
+++ Section03/Lec01/LinearSearch.cpp
@@ -0,0 +1,12 @@
+//Link: https://www.naukri.com/code360/problems/linear-search_6922070?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    // Write your code here.
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==num){
+            return i;
+        }
+    }return -1;   //after iterating through the whole array i knew that num is not prsent in array so return -1.
+  
+}
