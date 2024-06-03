ca097c315ce8ae3ca855343347871d5aac0457ed
Sejal Kaur <sejalkaur2004@gmail.com>
2024-06-03 17:33:50 +0530

    Create Q2.cpp

---
+++ Section01/Step1.5/Q2.cpp
@@ -0,0 +1,7 @@
+//Link: https://www.geeksforgeeks.org/problems/print-gfg-n-times/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-gfg-n-times
+ void printGfg(int N) {
+        // Code here
+        for(int i=0;i<N;i++){
+            cout<< "GFG ";
+        }
+    }
