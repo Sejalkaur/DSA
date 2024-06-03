4c02a2f4391a50a0fd2ce025de4fa1f921837ac2
Sejal Kaur <sejalkaur2004@gmail.com>
2024-06-03 17:31:50 +0530

    Create Q9.cpp

---
+++ Section01/Step1.5/Q9.cpp
@@ -0,0 +1,13 @@
+//link: https://leetcode.com/problems/fibonacci-number/description/
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1){
+            return n;
+        }else{
+            return fib(n - 1) +fib( n - 2);
+        }
+        }
+        
+    
+};
