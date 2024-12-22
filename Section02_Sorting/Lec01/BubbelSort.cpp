0612ebf036eb69c25bb3a9d2fdc724cecdae2c6d
Sejal Kaur <sejalkaur2004@gmail.com>
2024-12-22 19:05:49 +0530

    Update BubbelSort.cpp

--- Section02_Sorting/Lec01/BubbelSort.cpp
+++ Section02_Sorting/Lec01/BubbelSort.cpp
@@ -1,12 +1,16 @@
 //Link: https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort
-oid bubbleSort(vector<int>& arr) {
+void bubbleSort(vector<int>& arr) {
         // Your code here
         int n = arr.size();
-        for(int iteration = 1; iteration < n ;iteration++){
-          
-            for(int j = 0;j < n - iteration;j++){
-              
-                if(arr[j] > arr[j + 1])swap(arr[j], arr[j+1]);
+        for(int it = 1; it < n ;it++){
+            bool swapped = false;
+            for(int j = 0;j < n - it;j++){
+                if(arr[j] > arr[j + 1]){
+                    swap(arr[j], arr[j+1]);
+                    swapped = true;
+                }
             }
+            if(swapped == false)break;
         }
+        
     }
