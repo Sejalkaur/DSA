2538fe9ee28e37d0cf5dd80b04ec2331754c3991
Sejal Kaur <sejalkaur2004@gmail.com>
2025-01-05 22:18:48 +0530

    Create twoSum2.cpp

---
+++ Section05_Strings[basic, med]/CodeStorywithMik/twoSum2.cpp
@@ -0,0 +1,15 @@
+//link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        //binary search
+        int l =0;int r = numbers.size() - 1;
+        while(l<= r){
+            if(numbers[l] + numbers[r] == target)return {l+1, r+1};
+            else if(numbers[l] + numbers[r] > target)r--;
+            else l++;
+
+        }
+        return {-1, -1};
+    }
+};
