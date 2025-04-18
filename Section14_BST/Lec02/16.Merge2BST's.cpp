fe25abad05aed46b4057cef0205a45ddbf86e1dd
Sejal Kaur <sejalkaur2004@gmail.com>
2025-04-18 13:06:43 +0530

    Update 16.Merge2BST's.cpp

--- Section14_BST/Lec02/16.Merge2BST's.cpp
+++ Section14_BST/Lec02/16.Merge2BST's.cpp
@@ -1,31 +1,22 @@
 //Link: https://www.geeksforgeeks.org/problems/merge-two-bst-s/1?page=1&category=Binary%20Search%20Tree&sortBy=submissions
 public:
   
-  void inorder(Node* root1, vector<int> &arr1){
-      if(root1 == NULL)return;
+  void inorder(Node* root, vector<int> &arr){
+      if(root == NULL)return;
       
-      inorder(root1->left, arr1);
-      arr1.push_back(root1->data);
-      inorder(root1->right, arr1);
+      inorder(root->left, arr);
+      arr.push_back(root->data);
+      inorder(root->right, arr);
   }
-  
-  void inorder2(Node* root2, vector<int> &arr2){
-      if(root2 == NULL)return;
-      
-      inorder2(root2->left, arr2);
-      arr2.push_back(root2->data);
-      inorder2(root2->right, arr2);
-  }
+
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
     vector<int> merge(Node *root1, Node *root2) {
         // Your code here
-        vector<int> arr1;
-        vector<int> arr2;
-        vector<int> ans;
+         vector<int> arr1, arr2, ans;
         
         inorder(root1, arr1);
-        inorder2(root2, arr2);
+        inorder(root2, arr2);
         
         int i = 0;
         int j = 0;
